6861892f56355ba3bf97657a93ca3fc74c751d00
Taylor Hanayik <hanayik@gmail.com>
2023-09-26 16:56:12 +0100

    add hover

--- desktop/src/ui/components/Tools.jsx
+++ desktop/src/ui/components/Tools.jsx
@@ -4,8 +4,12 @@ import ZoomInIcon from '@mui/icons-material/ZoomIn';
 import PhotoSizeSelectSmallIcon from '@mui/icons-material/PhotoSizeSelectSmall';
 import DrawIcon from '@mui/icons-material/Draw';
 import StraightenIcon from '@mui/icons-material/Straighten';
+import ReplayIcon from '@mui/icons-material/Replay';
 
-function ToolButton({icon, ...props}){
+function ToolButton({icon, active, ...props}){
+    // background color state
+    const [bgColor, setBgColor] = useState('white')
+    const [activeColor, setActiveColor] = useState('lightgray')
     return (
         <div
         style={{
@@ -13,8 +17,12 @@ function ToolButton({icon, ...props}){
             flexDirection: 'row',
             minHeight: '0px',
             minWidth: '0px',
+            backgroundColor: active ? activeColor : bgColor,
+            borderRadius: '5px',
             ...props
         }}
+        onMouseEnter={() => setBgColor('lightgray')}
+        onMouseLeave={() => setBgColor('white')}
         >
             {icon}
         </div>
@@ -62,11 +70,18 @@ export function Tools({...props }){
         }}
         >
             <ToolButtonGroup>
-                <ToolButton icon={<AddIcon fontSize='large' />} />
+                {/* crosshair only... right click does nothing */}
+                <ToolButton icon={<AddIcon fontSize='large' sx={{color:'black'}} />} />
+                {/* zoom/pan */}
                 <ToolButton icon={<ZoomInIcon fontSize='large' />} />
+                {/* windowing/intensity selection */}
                 <ToolButton icon={<PhotoSizeSelectSmallIcon fontSize='large' />} />
+                {/* draw mode */}
                 <ToolButton icon={<DrawIcon fontSize='large' />} />
+                {/* ruler */}
                 <ToolButton icon={<StraightenIcon fontSize='large' />} />
+                {/* reset */}
+                <ToolButton icon={<ReplayIcon fontSize='large' />} />
             </ToolButtonGroup>
         
         </div>
