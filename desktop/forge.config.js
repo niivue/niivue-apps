4262d4aeb1bc45b292b1ae808f92ba3c0ca85a22
Taylor Hanayik <hanayik@gmail.com>
2023-10-30 12:09:15 +0000

    add publisher

--- desktop/forge.config.js
+++ desktop/forge.config.js
@@ -6,20 +6,20 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
+      platforms: ['darwin', 'linux'],
     },
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'niivue',
+          name: 'niivue-apps'
+        },
+        prerelease: true
+      }
+    }
+  ]
 };
